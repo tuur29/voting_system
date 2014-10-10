6ebfdc50f39811cf4731753d9a9b9c891f7c7a89
Tuur Lievens <tuur.lievens@hotmail.com>
2014-10-10 16:20:02 +0200

    reset to default options

--- options.php
+++ options.php
@@ -1,11 +1,11 @@
 <?php
 	$pass = "pass";				// password used to login on the results page
-	$lang = "nl";				// "en" or "nl" (add your own language in assets/language.php)
+	$lang = "en";				// "en" or "nl" (add your own language in assets/language.php)
 
 	// Advanced options:
 	$folder = 'round';			// the name of the active voting round
 	$separator = ";";			// a non-numeric character used to separate votes
-	$resultslink = false;		// show link to results after voting
+	$resultslink = true;		// show link to results after voting
 	$livetoggle = false;		// show pauze live refresh button on results page
 	$refreshinterval= 1500;		// the interval at which clients check for new results
 	$seo = true;				// seo makes the use of ".php" in url's optional (you need to load "mod_rewrite" in apache)
